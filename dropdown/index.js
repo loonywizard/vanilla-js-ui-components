2bdfdffd0855fa001e58df2d49d2caa4adbbb794
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-11-18 00:11:30 +0300

    add dropdown item selection & add one more dropdown to page

--- dropdown/index.js
+++ dropdown/index.js
@@ -1,16 +1,27 @@
 // TODO:
-// write option to dropdown button
-// close on item select
 // close on click outside
+// use event delegation!
 
 function addEventListenersForDropdown(dropdown) {
   const dropdownButton = dropdown.getElementsByClassName('dropdown-button')[0]
   const dropdownContent = dropdown.getElementsByClassName('dropdown-content')[0]
+  const dropdownOptions = [...dropdown.getElementsByClassName('dropdown-option')]
 
-  if (!dropdownButton || !dropdownContent) return
+  if (!dropdownButton || !dropdownContent || !dropdownOptions) return
 
   dropdownButton.addEventListener('click', function onDropdownButtonClick() {
     dropdownContent.classList.toggle('dropdown-content-open')
+  })
+
+  dropdownOptions.forEach((dropdownOption) => {
+    dropdownOption.addEventListener('click', function onDropdownOptionClick() {
+      dropdownContent.classList.remove('dropdown-content-open')
+      dropdownOptions.forEach(
+        (_innerDropdownOption) => _innerDropdownOption.classList.remove('selected-dropdown-option')
+      )
+      dropdownOption.classList.add('selected-dropdown-option')
+      dropdownButton.innerHTML = `Selected option: ${dropdownOption.dataset.optionValue}`
+    })
   })
 }
 
